444d5e675f0ff8ecc6b69db3a931df4866126067
lauragift21 <laurandidi21@gmail.com>
2022-03-22 07:20:06 +0100

    push solution 01_06

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>Gift Egwuenu</h1>
+    <p>Gift Egwuenu is a Developer Advocate at Cloudflare
+      and a LinkedIn Learning Instructor.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style>
+body {
+  border: 8px solid salmon;
+  margin: 0;
+  height: 100vh;
+}
+.container {
+  font-size: 30px;
+  text-align: center;
+}
+</style>
