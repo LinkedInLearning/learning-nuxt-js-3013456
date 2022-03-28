3662476f53221277e9857e8f91f58ddd49ad5de8
lauragift21 <laurandidi21@gmail.com>
2022-03-28 07:00:25 +0200

    add end state of 02_01

---
+++ pages/User/profile.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Profile.vue</p>
+</template>
---
+++ pages/user.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>This is a parent view!</p>
+    <nuxt-link to="/user/post">Post.vue</nuxt-link>
+    <nuxt-link to="/user/profile">Profile.vue</nuxt-link>
+    <nuxt-child />
+  </div>
+</template>
