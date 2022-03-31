cc75705362051ad60463e365b19a7fa2c2cc439a
lauragift21 <laurandidi21@gmail.com>
2022-03-31 07:44:21 +0200

    end state for 02_03

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Hello World!</h1>
+  <div>
+    <h1>Rivers in the World!</h1>
+    <ul>
+      <li v-for="(river, index) in rivers" :key="`river-${index}`">
+        {{ river.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      rivers: []
+    }
+  },
+  async fetch () {
+    const res = await this.$http.$get('https://api.nuxtjs.dev/rivers')
+    this.rivers = res
+  }
+
+  // async asyncData ({ $http }) {
+  //   // https://api.nuxtjs.dev/rivers
+  //   const res = await $http.$get('https://api.nuxtjs.dev/rivers')
+  //   return {
+  //     rivers: res
+  //   }
+  // }
 }
 </script>
