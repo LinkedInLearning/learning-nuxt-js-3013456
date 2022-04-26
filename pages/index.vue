621b221ed8ff1a8eced8fd638c1506c72e9a2aaa
lauragift21 <laurandidi21@gmail.com>
2022-04-26 08:10:08 +0200

    add-endstate for 02_03

--- pages/index.vue
+++ pages/index.vue
@@ -12,22 +12,13 @@
 <script>
 export default {
   name: 'IndexPage',
-  data () {
-    return {
-      rivers: []
-    }
-  },
-  async fetch () {
-    const res = await this.$http.$get('https://api.nuxtjs.dev/rivers')
-    this.rivers = res
-  }
 
-  // async asyncData ({ $http }) {
-  //   // https://api.nuxtjs.dev/rivers
-  //   const res = await $http.$get('https://api.nuxtjs.dev/rivers')
-  //   return {
-  //     rivers: res
-  //   }
-  // }
+  async asyncData ({ $http }) {
+    // https://api.nuxtjs.dev/rivers
+    const res = await $http.$get('https://api.nuxtjs.dev/rivers')
+    return {
+      rivers: res
+    }
+  }
 }
 </script>
